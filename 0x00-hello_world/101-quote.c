df98f2f55b56760096cf10215c208725558f255c
louisamen <amenlouis28@gmail.com>
2023-06-15 04:14:51 -0700

    best codes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - Prints the size of various types based on
  *        the computer it is compiled and run on..
  *
- * Return: Always 0.
+ * Return: Always 1.
  */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful" - Dora Korpar, 2015-10-19\n" 59);
+	"and that piece of art is useful" - Dora Korpar, 2015-10-19\n," 59);
 
 	return (1);
 }
